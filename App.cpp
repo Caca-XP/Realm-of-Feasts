d29fe03420583c6175750c98f66ecd7809a61f83
Natasha Prasetyo <prana445@student.otago.ac.nz>
2024-07-22 21:09:17 +1200

    App

---
+++ App.cpp
@@ -0,0 +1,80 @@
+# include <iostream>
+
+# include <string>
+
+# include <fstream>
+
+# include "Recipes.cpp"
+
+using namespace std;
+
+/// @todo find number of recipes in data
+const int numRecipes = 1;
+Recipes allRecipes[numRecipes];
+
+void setup(){
+// read file and initialise allRecipes
+    allRecipes[0] = Recipes("Pasta", "Italian", "A simple pasta recipe", "Pasta, Tomato Sauce, Cheese", "Boil pasta, add sauce, add cheese", "30 minutes", "Easy");
+}
+
+void display(Recipes recipes[]){
+    int length = sizeof(recipes) / sizeof(Recipes);
+    for (int i = 0; i < length; i++){
+        cout << recipes[i].getName() << endl;
+    }
+}
+
+
+
+void quit(){
+    cout << "Goodbye!" << endl;
+    exit(0);
+}
+
+
+void options(){
+    printf("1. Display all recipes\n");
+    printf("2. Search for a recipe\n");
+    printf("3. Sort recipes\n");
+    printf("4. Filter recipes\n");
+    printf("5. Search by ingredient\n");
+    printf("6. Random recipe\n");
+    printf("7. Quit\n");
+    int choice;
+    cin >> choice;
+    if (choice == 1){
+        display(allRecipes);
+    }
+    else if (choice == 2){
+        // search();
+    }
+    else if (choice == 3){
+        // sort();
+    }
+    else if (choice == 4){
+        // filter();
+    }
+    else if (choice == 5){
+        // searchByIngredient();
+    }
+    else if (choice == 6){
+        // random();
+    }
+    else if (choice == 7){
+        quit();
+    }
+    else{
+        cout << "Invalid choice. Please try again." << endl;
+    }
+
+}
+
+
+int main(){
+    // read and initialize the data for recipes
+    setup();
+    options();
+
+    // 
+    return 0;
+}
