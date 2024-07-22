93fdd5641a05997a0787c1161e3dba6b684c42af
Nadia Prasetyo <prana001@student.otago.ac.nz>
2024-07-22 22:31:23 +1200

    Changing implementation of the abstraction of recipes support class to be implemented in the app class

--- App.cpp
+++ App.cpp
@@ -5,8 +5,70 @@
 
 using namespace std;
 
+/*Default Constructor */
+    Recipes::Recipes(){
+    };
+
+    /*Defined Constructor*/
+    Recipes::Recipes(string nm, string ser, string desc, string ing, string ins, string t, string lv) {
+        name = nm;
+        series = ser;
+        description = desc;
+        ingredients = ing;
+        instructions = ins;
+        time = t;
+        difficulty = lv;
+
+        
+    }
+
+    /*Destructor code (idk what this is for)*/
+    Recipes::~Recipes() {
+        // Destructor code here
+    }
+
+    /* Accessor function to get the name of the recipe
+    */
+    string Recipes::getName() {
+        return name;
+    }
+
+    /* Accessor function to get the description of the recipe
+    */
+    string Recipes::getDescription() {
+        return description;
+    }
+
+    /* Accessor function to get the ingredients of the recipe
+    */
+    string Recipes::getIngredients() {
+        return ingredients;
+    }
+
+    /* Accessor function to get the instructions of the recipe
+    */
+    string Recipes::getInstructions() {
+        return instructions;
+    }
+
+    /* Accessor function to get the time of the recipe
+    */
+    string Recipes::getTime() {
+        return time;
+    }
+
+    /* Accessor function to get the difficulty of the recipe
+    */
+    string Recipes::getDifficulty() {
+        return difficulty;
+    }
+
+
+
 /// @todo find number of recipes in data
 const int numRecipes = 1;
+
+Recipes pasta = Recipes("Pasta", "Italian", "A simple pasta recipe", "Pasta, Tomato Sauce, Cheese", "Boil pasta, add sauce, add cheese", "30 minutes", "Easy");
 
 Recipes allRecipes[numRecipes];
 
