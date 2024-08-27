f77771d2430d07ec98d35a560d42cb138711e7c9
Beleifte <ishbe125@student.otago.ac.nz>
2024-08-27 15:37:04 +1200

    readme and index page changes

--- src/Mainpage.h
+++ src/Mainpage.h
@@ -17,4 +17,61 @@
  *
  * Bernice Ishumi
  *\section usage How to Use Realm of Feasts
+* - A C/C++ compiler is required to run this Application.
+*\section Running the App
+*1. Compile and run App.cpp
+*2. Read the instructions displayed
+*3. Read the options displayed
+*4. Choose one of the options to begin interacting with the application.
+*5. Iteract with app to enjoy the quest of finding a fantastical recipe
+*\section Testing
+*The application was tested using Unit Tests.The tests can be found in the RealmOfFeastsTest folder in the tests directory.
+*
+*\section Version History
+*
+*\section Alpha
+*Basic functionality of the App.
+*
+*Released on:
+*
+*Tag:
+*
+*Release:
+*
+*\section Beta
+*Currently Unreleased
+*
+*\section Final Product
+*Currently Unreleased
+*
+*\section Dependencies
+*The software relies on the following C/C++ packages:
+*
+*-- `<iostream>`
+*
+*-- `<string>`
+*
+*-- `<fstream>`
+*
+*-- `<array>`
+*
+*-- `<sstream>`
+*
+*-- `<iterator>`
+*
+*-- `<algorithm>`
+*
+*-- `<chrono>`
+*
+*-- `<thread>`
+*
+*-- `<cstdlib>`
+*
+*-- `<vector>`
+*
+*-- `pch.h`
+*
+*-- `CppUnitTest.h`
+*
+*Ensure that these packages and libraries are available in you C/C++ environment.
 */
