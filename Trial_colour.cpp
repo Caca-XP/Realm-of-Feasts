53d69e4c83f7da9209e6d61b47dc3ca672e274c6
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-09-10 16:46:22 +1200

    colours with get

--- Trial_colour.cpp
+++ Trial_colour.cpp
@@ -7,10 +7,27 @@ void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+// Function to get the current text color
+int getCurrentColor() {
+    // Get the handle to the console output
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Structure to hold console buffer information
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    // Get the current console screen buffer information
+    if (GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+        // Extract and return the current text attribute (color)
+        return csbi.wAttributes;
+    }
+
+    return -1; // Return -1 if getting the information fails
+}
+
 int main() {
     // try all colours
     for (int i = 0; i < 16; i++) {
         setColor(i);
-        cout << "Colour " << i << endl;
+        printf("This is colour %d\n", i);
     }
 }
